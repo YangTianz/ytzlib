155da1a85e8e0acdbe36020f1d95d8463497b790
YangTianz <tingerytz@gmail.com>
2024-06-10 23:33:29 +0800

    增加正则表达式

--- tests/test_process_path.py
+++ tests/test_process_path.py
@@ -1,49 +1,81 @@
-from ytzlib.path_helper import process_path
+from ytzlib.path_helper import search_by_exts, search_by_regex
 
 
-def test_process_path_py():
-    ret = process_path(
+def test_search_by_exts_py():
+    ret = search_by_exts(
         ["tests/test_directory_tree"],
         ["py"],
     )
     assert len(ret) == 4
 
 
-def test_process_path_txt():
-    ret = process_path(
+def test_search_by_exts_txt():
+    ret = search_by_exts(
         ["tests/test_directory_tree"],
         ["txt"],
     )
     assert len(ret) == 6
 
 
-def test_process_path_txt_and_py():
-    ret = process_path(
+def test_search_by_exts_txt_and_py():
+    ret = search_by_exts(
         ["tests/test_directory_tree"],
         ["txt", "py"],
     )
     assert len(ret) == 10
 
 
-def test_process_path_md():
-    ret = process_path(
+def test_search_by_exts_md():
+    ret = search_by_exts(
         ["tests/test_directory_tree"],
         ["md"],
     )
     assert len(ret) == 2
 
 
-def test_process_path_tx():
-    ret = process_path(
+def test_search_by_exts_tx():
+    ret = search_by_exts(
         ["tests/test_directory_tree"],
         ["tx"],
     )
     assert len(ret) == 0
 
 
-def test_process_path_pyc():
-    ret = process_path(
+def test_search_by_exts_pyc():
+    ret = search_by_exts(
         ["tests/test_directory_tree"],
         ["tx"],
     )
     assert len(ret) == 0
+
+
+def test_search_by_regex_1():
+    ret = search_by_regex(
+        ["tests/test_directory_tree"],
+        ["[a-z]*"],
+    )
+    assert len(ret) == 12
+
+
+def test_search_by_regex_1():
+    ret = search_by_regex(
+        ["tests/test_directory_tree"],
+        ["[a-z]*[2]+"],
+    )
+    assert len(ret) == 5
+
+
+def test_search_by_regex_2():
+    ret = search_by_regex(
+        ["tests/test_directory_tree"],
+        ["[a-z]*[12]\.py"],
+    )
+    assert len(ret) == 2
+
+
+def test_search_by_regex_2():
+    ret = search_by_regex(
+        ["tests/test_directory_tree"],
+        ["[a-z]*[2]-[01]+\.py"],
+    )
+    assert len(ret) == 1
--- ytzlib/path_helper.py
+++ ytzlib/path_helper.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 
 def process_path(input_paths: list[str], exts: list[str]) -> list[str]:
@@ -14,5 +15,40 @@ def process_path(input_paths: list[str], exts: list[str]) -> list[str]:
         else:
             for ext in exts:
                 if input_path.endswith(ext):
+                    ret.append(input_path)
+    return ret
+
+
+def search_by_exts(input_paths: list[str], exts: list[str]) -> list[str]:
+    ret = []
+    for input_path in input_paths:
+        if os.path.isdir(input_path):
+            for path, _, file_names in os.walk(input_path):
+                for file_name in file_names:
+                    for ext in exts:
+                        if file_name.endswith(ext):
+                            ret.append(os.path.join(path, file_name))
+        else:
+            for ext in exts:
+                if input_path.endswith(ext):
+                    ret.append(input_path)
+    return ret
+
+
+def search_by_regex(
+    input_paths: list[str], regex_patterns: list[str]
+) -> list[str]:
+    patterns = [re.compile(p) for p in regex_patterns]
+    ret = []
+    for input_path in input_paths:
+        if os.path.isdir(input_path):
+            for path, _, file_names in os.walk(input_path):
+                for file_name in file_names:
+                    for pattern in patterns:
+                        if pattern.match(file_name) is not None:
+                            ret.append(os.path.join(path, file_name))
+        else:
+            for pattern in patterns:
+                if pattern.match(input_paths) is not None:
                     ret.append(input_path)
     return ret
